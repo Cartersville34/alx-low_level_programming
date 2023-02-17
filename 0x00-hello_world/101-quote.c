fb414b56f3d39073911f66f1c6fba7c59090bb4a
Cartersville34 <amupatrick@gmail.com>
2023-02-16 18:13:08 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
-  
  /** 
   * main - prints a standard error 
   * 
   * Return: Always 1 (Success) 
   */ 
-  
  int main(void) 
  { 
- write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+ write(2,
+	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
  return (1); 
  }
