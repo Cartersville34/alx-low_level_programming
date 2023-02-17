94323620bd5a0ed81adadcd4df9b93015392c476
Cartersville34 <amupatrick@gmail.com>
2023-02-16 20:41:11 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
- */
+* main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+* followed by a newline, to standard error.
+* Return: Always 1.
+*/
 
 int main(void)
 {
