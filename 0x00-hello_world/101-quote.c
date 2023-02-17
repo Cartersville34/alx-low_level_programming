feccf8954d90ec2ad1466c0eb3205f49be0dedea
Cartersville34 <amupatrick@gmail.com>
2023-02-17 02:30:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
-*main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+*main -prints "and that piece of art is useful" -Dora Korpar, 2015-10-19",
 *followed by a newline, to standard error.
-*Return: Always 1.
+*Return:Always 1.
 */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+return(1);
 } 
