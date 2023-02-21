88bace447243eb635abe7a400eb1723ad2c05553
Cartersville34 <amupatrick@gmail.com>
2023-02-20 16:46:05 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        char lower = 'a';
+
+        while (lower < = 'z')
+{
+	                putchar(lower);
+	                lower++;
+}
+                putchar('\n');
+
+        return (0);
+}
